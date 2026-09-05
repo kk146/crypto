1c3590a7effd0c885bdee5f3421dbf975bd37e5e
kk146 <kokilamishra1@gmail.com>
2026-09-05 22:45:25 +0530

    Pass currency state to Dashboard components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,21 @@
 import Header from "../components/Header";
 import ChartSection from "../components/ChartSection";
 import Portfolio from "../components/Portfolio";
-import ExchangeCoins from "../components/ExchangeCoins";
+import ExchangeCard from "../components/ExchangeCard";
 import MarketCapList from "../components/MarketCapList";
+import { useState } from "react";
 
 function Dashboard() {
+  const [currency, setCurrency] = useState("usd");
+
   return (
     <div className="bg-slate-100 min-h-screen p-6">
       {/* HEADER */}
       <div className="mb-4">
-        <Header />
+        <Header
+          currency={currency}
+          setCurrency={setCurrency}
+        />
       </div>
 
       {/* MAIN DASHBOARD */}
@@ -17,18 +23,18 @@ function Dashboard() {
         {/* LEFT SIDE */}
         <div className="col-span-9">
           {/* CRYPTO GRAPH */}
-          <ChartSection />
+          <ChartSection currency={currency} />
 
           {/* PORTFOLIO + EXCHANGE */}
           <div className="grid grid-cols-2 gap-4 mt-4">
             <Portfolio />
-            <ExchangeCoins />
+            <ExchangeCard />
           </div>
         </div>
 
         {/* RIGHT SIDE */}
         <div className="col-span-3">
-          <MarketCapList />
+          <MarketCapList currency={currency} />
         </div>
       </div>
     </div>
