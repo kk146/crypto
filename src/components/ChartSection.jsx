8a19f4e1c8c0b78d532df9ee77b4458af9bb3bf2
kk146 <kokilamishra1@gmail.com>
2026-09-07 22:42:33 +0530

    Refactor ChartSection to simplify coin selection
    Removed the toggleCoin function and passed setSelectedCoins directly to ChartControls.

--- src/components/ChartSection.jsx
+++ src/components/ChartSection.jsx
@@ -6,39 +6,21 @@ function ChartSection({ currency = "usd" }) {
   const [chartType, setChartType] = useState("line");
   const [activeRange, setActiveRange] = useState("1W");
 
-  // Default cryptocurrency
   const [selectedCoins, setSelectedCoins] = useState([
     "Ethereum",
   ]);
 
-  const toggleCoin = (coin) => {
-    setSelectedCoins((current) => {
-      // Don't allow the user to remove the last coin
-      if (current.includes(coin)) {
-        if (current.length === 1) {
-          return current;
-        }
-
-        return current.filter((item) => item !== coin);
-      }
-
-      return [...current, coin];
-    });
-  };
-
   return (
     <div className="w-full rounded-xl bg-white p-4 shadow-sm">
-      {/* Chart controls */}
       <ChartControls
         chartType={chartType}
         setChartType={setChartType}
         activeRange={activeRange}
         setActiveRange={setActiveRange}
         selectedCoins={selectedCoins}
-        toggleCoin={toggleCoin}
+        setSelectedCoins={setSelectedCoins}
       />
 
-      {/* Price chart */}
       <div className="mt-4">
         <PriceChart
           chartType={chartType}
